90b213281b2ea6a9692d4d49cd39f9fac0f271dd
serhatbek <serhat.bekk@gmail.com>
2021-02-03 21:10:19 +0300

    final

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,23 +1,42 @@
 const tabs = document.querySelectorAll('[data-tab-btn]');
 const tabsContent = document.querySelectorAll('[data-tab-content]');
 
-tabs.forEach((tab) => {
-  tab.addEventListener('click', (e) => {
-    showTabPanel(e);
-  });
+tabs.forEach((tabBtn) => {
+  tabBtn.addEventListener('click', (e) => showTabPanel(e));
 });
 
 function showTabPanel(e) {
-  const targetId = e.target.dataset.tabBtn,
-    target = document.getElementById(targetId);
+  const tabId = e.target.dataset.tabBtn,
+    target = document.getElementById(tabId);
 
   tabs.forEach((tabBtn) => {
     tabBtn.classList.remove('active');
   });
-  e.target.classList.add('active');
 
   tabsContent.forEach((tabContent) => {
     tabContent.classList.remove('active');
   });
+  e.target.classList.add('active');
   target.classList.add('active');
 }
+
+// tabs.forEach((tab) => {
+//   tab.addEventListener('click', (e) => {
+//     showTabPanel(e);
+//   });
+// });
+
+// function showTabPanel(e) {
+//   const targetId = e.target.dataset.tabBtn,
+//     target = document.getElementById(targetId);
+
+//   tabs.forEach((tabBtn) => {
+//     tabBtn.classList.remove('active');
+//   });
+//   e.target.classList.add('active');
+
+//   tabsContent.forEach((tabContent) => {
+//     tabContent.classList.remove('active');
+//   });
+//   target.classList.add('active');
+// }
